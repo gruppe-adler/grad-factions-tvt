ceb5e3bac890da0f90f8a02a893a25369a01d460
McDiod <hobbeldobbel@gmail.com>
2017-05-17 01:56:05 +0200

    add missing includes

--- loadouts.hpp
+++ loadouts.hpp
@@ -2,6 +2,12 @@
 
 #include "factions\ger_heer_w.hpp"
 #include "factions\ger_heer_d.hpp"
+
+#include "ind_eastern_nationalists.hpp"
+#include "ind_me_terrorists.hpp"
+#include "ind_pmc_special.hpp"
+
 #include "factions\rus_msv_w.hpp"
 #include "factions\rus_msv_d.hpp"
+
 #include "factions\us_army.hpp"
