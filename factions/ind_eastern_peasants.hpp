b21a2562426e6ce89c8124e9b0c35a1749765be8
nomisum <nomisum@gmail.com>
2021-08-03 20:57:24 +0200

    lop replacements for tkk, pmc west, me terrors and me para (thx b4dhorse)

--- factions/ind_eastern_peasants.hpp
+++ factions/ind_eastern_peasants.hpp
@@ -1,25 +1,27 @@
 class ind_eastern_peasants: ind_me_terrorists {
 	class AllUnits: AllUnits {
 		uniform[] = {
+			"UK3CB_CHC_C_U_ACTIVIST_01",
+			"UK3CB_CHC_C_U_ACTIVIST_04",
+			"UK3CB_CHC_C_U_CIT_01",
+			"UK3CB_CHC_C_U_CIT_03",
+			"UK3CB_CHC_C_U_CIT_04",
+			"UK3CB_CHC_C_U_WORK_02",
+			"UK3CB_CHC_C_U_VILL_03",
 			"UK3CB_CHC_C_U_WOOD_01",
 			"UK3CB_CHC_C_U_WOOD_02",
 			"UK3CB_CHC_C_U_WOOD_03",
-			"UK3CB_CHC_C_U_WOOD_04",
-			"UK3CB_CHC_C_U_CIT_01",
-			"UK3CB_CHC_C_U_CIT_02",
-			"UK3CB_CHC_C_U_CIT_03",
-			"UK3CB_CHC_C_U_CIT_04",
-			"UK3CB_CHC_C_U_VILL_01",
-			"UK3CB_CHC_C_U_VILL_02",
-			"UK3CB_CHC_C_U_VILL_03",
-			"UK3CB_CHC_C_U_VILL_04"
+			"UK3CB_CHC_C_U_WOOD_04"
 		};
 		headgear[] = {
 			"",
 			"UK3CB_H_Ushanka_Cap_01",
-			"rhs_ushanka",
 			"UK3CB_H_Villager_Cap_01",
-			"H_Hat_brown"
+			"UK3CB_H_Beanie_02_Blk",
+			"UK3CB_H_Beanie_02_Brn",
+			"UK3CB_H_Beanie_02_Camo",
+			"H_Hat_brown",
+			"rhs_ushanka"
 		};
 	};
 
